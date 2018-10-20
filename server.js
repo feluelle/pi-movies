ae09e91d5dffe5544e036f35623035eafa1abbfd
feluelle <felix.uellendall@gmx.de>
2018-10-20 13:57:44 +0200

    Adds mongodb
    - adds mongodb and body-parser deps
    - adds GET /movies endpoint for getting all movies in the db
    - adds POST /movies endpoint for adding a new movie into the db
    - adds connection to the mongodb

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
 'use strict';
 
 const express = require('express');
+const mongodb = require('mongodb');
+const bodyParser = require('body-parser');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
+var moviedb = undefined;
 
 const app = express();
+
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello World')
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.get('/movies', (req, res) => {
+  moviedb.collection('movies').find().toArray(function(err, results) {
+    if (err) return console.log(err)
+    res.send(results)
+  })
+});
+
+app.post('/movies', (req, res) => {
+  moviedb.collection('movies').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log(`Saved ${req.body} to database!`)
+    res.redirect('/movies')
+  })
+})
+
+mongodb.MongoClient.connect('mongodb://moviedb:27017/moviedb', (err, client) => {
+  if (err) return console.log(err)
+  moviedb = client.db('moviedb')
+  app.listen(PORT, HOST, () => {
+    console.log(`Running on http://${HOST}:${PORT}`);
+  })
+})
